5e9ba3e3baeddb8e50037197aae55f230a351357
Vanshika jain <140872033+kavya-sys@users.noreply.github.com>
2025-07-13 09:24:21 +0000

    conversion of datatypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,11 @@
+const account_Id=144453   //change nhi kia ja skta
+let accountEmail="hitesh@google.com"
+var accountPassword="12345"
+// account_Id=2 not allowed
+accountEmail="hc@hc.com"
+accountPassword ="22233222"
+accountcity="Bengaluru "
+console.log(account_Id);
+/* 
+Prefer not to use var because of issue in block scope and function block 
+*/
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all JS code as newer version
+// alert("hello")  //we are using nodejs, not browser
+// console.log(3+3); console.log("hitesh") //maintain code readabilityfff
+// number => 2 to power 53
+//bigInt
+// string=> ""
+//boolean => true/false
+//null => standalone value
+
+// undefined => initiolsie nhi kia hai 
+// symbol => unqiue
+//object
+// console.log(typeof "hietsh"); // stirng
+// console.log(typeof undefined); //undefined
+// console.log(typeof null); //object
+
