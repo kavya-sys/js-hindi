d7dd3809b52527229db8dbf2d81a2ef20a6bf46e
Vanshika jain <140872033+kavya-sys@users.noreply.github.com>
2025-07-13 10:03:30 +0000

    datatypes_summary

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,12 @@
+// console.log(2 >1)
+// console.log(2>=1)
+// console.log(2 <1)
+// console.log(2==1)
+// // console.log(2!=1)
+// console.log("2" > 1);
+// console.log("02" > 1);
+//  console.log(null>0) //false
+// console.log(null ==0) //false
+// console.log(null >= 0) //true
+// strict check === also check data type
+// console.log("2" ===2);  false
---
+++ 01_basic/datatypes_summary.js
@@ -0,0 +1,23 @@
+// Primitive
+// 7 types : String ,Number, Boolean , null, undefined, Symbol, BigInt
+// const score= 100 //Number 
+// const scoreValue =100.3 //Number
+// const isLoggedIn =fasle //Boolean
+// const outsideTemp= null;
+// let userEmail; //undefined
+// const id=Symbol('123')
+// const anotherId=Symbol('123')
+// console.log(id === anotherId); //false
+// const bigNumber= 122321322332214n;//BigInt //typeof undefined
+
+// Refernce (Non Primitive)
+const heroes =["shaktiman", "naagraj", "doga"]; //typeof ata hai object
+let myObj ={
+    name:"hitesh",
+    age: 22,
+} // tyoeof ata hai obejct
+const myFunction = function(){
+    console.log("Hello world");
+} //type ata hai function
+// Array, Obejcts,Function
+console.log(typeof myFunction);
