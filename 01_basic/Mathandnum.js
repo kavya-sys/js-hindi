5ec3ed315c5e24fce3da076fe0322881db8ed2d9
Vanshika jain <140872033+kavya-sys@users.noreply.github.com>
2025-07-14 14:00:35 +0000

    dates

--- 01_basic/Mathandnum.js
+++ 01_basic/Mathandnum.js
@@ -23,6 +23,6 @@
 // console.log((Math.random()*10) +1)
 
 
-const min=10
-const max=20
-console.log(Math.floor(Math.random()*(max-min+1))+min)
+// const min=10
+// const max=20
+// console.log(Math.floor(Math.random()*(max-min+1))+min)
